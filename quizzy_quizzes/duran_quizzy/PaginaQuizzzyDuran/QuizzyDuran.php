f84d354094d9417cb48d1c0350688b266d6acf6e
87602 <93334168+87602@users.noreply.github.com>
2022-02-14 12:50:39 +0100

    Duran 0.0.3

--- quizzy_quizzes/duran_quizzy/PaginaQuizzzyDuran/QuizzyDuran.php
+++ quizzy_quizzes/duran_quizzy/PaginaQuizzzyDuran/QuizzyDuran.php
@@ -8,9 +8,15 @@
     <link rel="stylesheet" href="style.css" media="all">
 </head>
 <body>
+<?php include './nav.php' ?>
     <header class="Header1 Header2">
         <img src="Afbeeldingen/Lord-Of-The-Rings.jpg" alt="">
         <img src="Afbeeldingen/branch.png" class="top  h-auto">
+        <div>
+            <h1 id="MainText">Welcome to my quiz</h1>
+            <h1 id="MainText">You think you can pass?</h1>
+            <H1 id="MainText">Do your best!</H1>
+        </div>
     </header>
 </body>
 </html>
