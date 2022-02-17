93d32fd2471142ee688e1506d85bd2f4706b354f
87602 <93334168+87602@users.noreply.github.com>
2022-02-17 10:54:10 +0100

    0.0.5

--- quizzy_duran.php
+++ quizzy_duran.php
@@ -6,15 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quizzy Lord Of The Rings</title>
-    <link rel="stylesheet" href="style.css" media="all">
-    <link rel="stylesheet" href="../../../css/nav_styles.css" media="all">
+    <link rel="stylesheet" href="css/style_duran.css" media="all">
+    <link rel="stylesheet" href="css/nav_styles.css" media="all">
 </head>
 
 <body>
+<?php include 'nav.php'?>
     <header class="Header1 Header2">
-        <img src="Afbeeldingen/Lord-Of-The-Rings.jpg" alt="">
-        <img src="Afbeeldingen/branch.png" class="top  h-auto">
-        <div>
+        <img src="Images/Lord-Of-The-Rings.jpg" alt="">
+        <img src="Images/branch.png" class="top  h-auto">
+        <div id="KopTekst">
             <h1 id="MainText">Welcome to my quiz</h1>
             <h1 id="MainText">You think you can pass?</h1>
             <H1 id="MainText">Do your best!</H1>
