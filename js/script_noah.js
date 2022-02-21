e3772b4f1175629cfebffaef5e57ab462465ad99
Araecy <74868163+Araecy@users.noreply.github.com>
2022-02-21 12:37:37 +0100

    Cool

--- js/script_noah.js
+++ js/script_noah.js
@@ -45,7 +45,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
                         + '<input type="radio" name="question'+i+'" value="'+letter+'">'
                         + letter + ': '
                         + questions[i].answers[letter]
-                    + '</label>'
+                    + '</label>' + '<br>'
                 );
             }
     
