28a962b4950bdcb1105339c969a264b0b0a52088
87602 <93334168+87602@users.noreply.github.com>
2022-03-07 12:36:45 +0100

    0.1.1

--- js/Duran.js
+++ js/Duran.js
@@ -17,6 +17,10 @@ function fout1(){
   document.getElementById("fout3").style='background-color: none;';
   document.getElementById("goed").style='background-color: none;';
   document.getElementById("quiz").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
+  if (score > 1){
+    score --;
+    return;
+  }
 }
 function fout2(){
   document.getElementById("fout2").style='background-color: red;';
@@ -24,6 +28,10 @@ function fout2(){
   document.getElementById("fout1").style='background-color: none;';
   document.getElementById("goed").style='background-color: none;';
   document.getElementById("quiz").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
+  if (score > 1){
+    score --;
+    return;
+  }
 }
 function fout3(){
   document.getElementById("fout3").style='background-color: red;';
@@ -31,6 +39,10 @@ function fout3(){
   document.getElementById("fout1").style='background-color: none;';
   document.getElementById("goed").style='background-color: none;';
   document.getElementById("quiz").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
+  if (score > 1){
+    score --;
+    return;
+  }
 }
 function next1(){
   document.getElementById('main').innerHTML = 
