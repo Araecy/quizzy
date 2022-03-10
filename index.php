de3947789ad6ab7934a62facc268d282138b13c3
87602 <93334168+87602@users.noreply.github.com>
2022-03-10 12:43:31 +0100

    edits home page

--- index.php
+++ index.php
@@ -11,5 +11,12 @@
 </head>
 <body>
     <?php include './nav.php'?>
+    <div class="parent">
+<div class="divTop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis reiciendis inventore magnam, iusto nulla in sapiente mollitia fugit provident! Quod perferendis dolores temporibus cum rem exercitationem ullam quaerat est.</div>
+<div class="divMidle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, consectetur accusamus, ea distinctio doloribus dolorum iusto nihil eos commodi eligendi sequi praesentium ratione qui quisquam? Nesciunt recusandae alias similique hic.</div>
+<div class="divLeft"></div>
+<div class="divRight"> </div>
+<div class="divBottom">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?</div>
+</div>
 </body>
 </html>
