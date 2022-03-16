0d8f43e3989746a68dc40aaa48f44252e0072f0a
Araecy <74868163+Araecy@users.noreply.github.com>
2022-03-16 20:06:18 +0100

    Update
    Still building on all my problems.

--- quizzy_noah.php
+++ quizzy_noah.php
@@ -1,66 +1,22 @@
 <!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="./css/style_noah.css">
-    <link rel="stylesheet" href="./css/nav_styles.css">
-    <script src="./js/script_noah.js" defer></script>
-    <title>Noah</title>
-</head>
-
-<body>
-    <?php include 'nav.php' ?>
-
-<!--     
-    <div id="app">
-        <h1 v-text="deckName"></h1>
-
-        <div class="user-input">
-            Type something <input type="text" v-model="userInput">
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Noah | Quizzy - Osu</title>
+        <link rel="stylesheet" href="css/style_noah.css">
+        <link rel="stylesheet" href="css/nav_styles.css">
+    </head>
+    <body>
+        <?php include 'nav.php'?>
+        <span id="scoreCounter" class="centerText">Score: 0</span>
+        <div id="firstDiv">
+            <h1>Osu quizzy</h1>
+            <div id="buttonDiv">
+                <button onclick="question(1)">Start quiz</button>
+            </div>
         </div>
-
-        <div @click="flipped = !flipped" class="card">
-            <h2 v-html="cards[index].front"></h2>
-            <h2 v-html="userInput == 'flip' || flipped ? '' : cards[index].front"></h2>
-            <div v-html="userInput == 'flip' || flipped ? cards[index].back : ''"></div>
-        </div>
-
-        <div class="navigation">
-            <button @click="move(-1)" type="button">&#8592;</button>
-            <button @click="move(1)" type="button">&#8594;</button>
-        </div>
-
-        <a :href="readMoreUrl">read more</a>
-    </div>
-
-    <script src="./js/data.js"></script>
-    <script src="https://unpkg.com/vue@3.0.7/dist/vue.global.js"></script>
-    <script>
-        const FlashCardApp = {
-            data() {
-                return {
-                    deckName: 'quizzy',
-                    cards,
-                    index: 0,
-                    flipped: false,
-                    readMoreUrl: 'https://osu.ppy.sh/home',
-                    userInput: 'default'
-                };
-            },
-            methods: {
-                move(change) {
-                    if (this.cards[this.index + change]) {
-                        this.index += change;
-                        this.flipped = false;
-                    }
-                }
-            }
-        };
-
-        const app = Vue.createApp(FlashCardApp).mount('#app');
-    </script> -->
-
-</body>
-
+        <script src="js/script_noah.js"></script>
+    </body>
 </html>
