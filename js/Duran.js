b68ea22ee925f7cd543576af4706849e59ec865c
87602 <93334168+87602@users.noreply.github.com>
2022-03-17 12:09:00 +0100

    1.5.0

--- js/Duran.js
+++ js/Duran.js
@@ -12,6 +12,7 @@ function Answer(){
     score ++;
   }
 }
+
 function answer1(){
   document.getElementById("answer1").style='background-color: red;';
   document.getElementById("quiz1").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
@@ -21,6 +22,7 @@ function answer1(){
   document.getElementById("answer2").onclick = "answered()";
   document.getElementById("answer3").onclick = "answered()";
 }
+
 function answer2(){
   document.getElementById("answer2").style='background-color: red;';
   document.getElementById("quiz1").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
@@ -30,6 +32,7 @@ function answer2(){
   document.getElementById("answer2").onclick = "answered()";
   document.getElementById("answer3").onclick = "answered()";
 }
+
 function answer3(){
   document.getElementById("answer3").style='background-color: red;';
   document.getElementById("quiz1").innerHTML = `<img id="fout" src="Images/wrong_Duran.gif" alt="">`;
@@ -39,15 +42,17 @@ function answer3(){
   document.getElementById("answer2").onclick = "answered()";
   document.getElementById("answer3").onclick = "answered()";
 }
+
 function Submit(){
   console.log(score+"/10");
 }
+
 function next1(){
   document.getElementById('main').innerHTML = 
   `
   <center>
   <h2>How manny times does Frodo fall down in The Lord of the Rings trilogy?</h2>
-  <h4><strong onclick="previous1()"><</strong>2/10<strong onclick="next2()">></strong></h4>
+  <h4><strong><</strong>2/10<strong onclick="next2()">></strong></h4>
   
    <div id="gif">
       <div id="quiz1"></div>
@@ -57,7 +62,7 @@ function next1(){
         <button onclick="answer2()" id="answer2">c)53 times</button> <button onclick="answer3()" id="answer3">d)25 times</button>
       </div>
       <div id="bottom">
-      <button onclick="previous1()" id="previous">Previous Question</button>
+
       <button onclick="next2()" id="next">Next Question</button>
       <br>
       <!--<button id="submit">Submit Quiz</button> -->
@@ -67,37 +72,13 @@ function next1(){
       <div id="results"></div>
 </center>`
 }
-// function previous1(){
-//   document.getElementById('main').innerHTML = 
-//   `
-//   <center>
-//   <h2>Which movie won the Academy Award for Best Picture?</h2>
-//   <h4>1/10<strong onclick="next1()">></strong></h4>
-  
-//    <div id="gif">
-//       <div id="quiz1"></div>
-//       <div id="Answers">
-//           <button onclick="answer1()" id="answer1">a)The Fellowship of the Ring</button> <button onclick="Answer()" id="Answer">b)The Return of the King</button>
-//           <br>
-//           <button onclick="answer2()" id="answer2">c)The Two Towers</button> <button onclick="answer3()" id="answer3">d)The Hobbit</button>
-//       </div>
-//       <div id="bottom">
-//       <!-- <button id="previous">Previous Question</button> -->
-//       <button onclick="next1()" id="next">Next Question</button>
-//       <br>
-//       <!--<button id="submit">Submit Quiz</button> -->
-//       </div>
-//       <div id="quiz2"></div>
-//       </div>
-//       <div id="results"></div>
-//   </center>`
-// }
+
 function next2(){
   document.getElementById('main').innerHTML =
   `
   <center>
   <h2>Where is Hobbiton located in real life?</h2>
-  <h4><strong onclick="previous2()"><</strong>3/10<strong onclick="next3()">></strong></h4>
+  <h4><strong><</strong>3/10<strong onclick="next3()">></strong></h4>
   
    <div id="gif">
       <div id="quiz1"></div>
@@ -107,7 +88,7 @@ function next2(){
         <button onclick="Answer()" id="Answer">c)Alexander Farm</button> <button onclick="answer3()" id="answer3">d)Hamilton</button>
       </div>
       <div id="bottom">
-      <button onclick="previous2()" id="previous">Previous Question</button>
+
       <button onclick="next3()" id="next">Next Question</button>
       <br>
       <!--<button id="submit">Submit Quiz</button> -->
@@ -117,37 +98,13 @@ function next2(){
       <div id="results"></div>
   </center>`
 }
-// function previous2(){
-//   document.getElementById('main').innerHTML = 
-//   `
-//   <center>
-//   <h2>How manny times does Frodo fall down in The Lord of the Rings trilogy?</h2>
-//   <h4><strong onclick="previous1()"><</strong>2/10<strong onclick="next2()">></strong></h4>
-  
-//    <div id="gif">
-//       <div id="quiz1"></div>
-//       <div id="Answers">
-//         <button onclick="Answer()" id="Answer">a)39 times</button> <button onclick="answer1()" id="answer1">b)5 times</button>
-//         <br>
-//         <button onclick="answer2()" id="answer2">c)53 times</button> <button onclick="answer3()" id="answer3">d)25 times</button>
-//       </div>
-//       <div id="bottom">
-//       <button onclick="previous1()" id="previous">Previous Question</button>
-//       <button onclick="next2()" id="next">Next Question</button>
-//       <br>
-//       <!--<button id="submit">Submit Quiz</button> -->
-//       </div>
-//       <div id="quiz2"></div>
-//       </div>
-//       <div id="results"></div>
-//   </center>`
-// }
+
 function next3(){
   document.getElementById('main').innerHTML =
   `
   <center>
   <h2>idk?</h2>
-  <h4><strong onclick="previous3()"><</strong>4/10<strong onclick="next4()">></strong></h4>
+  <h4><strong><</strong>4/10<strong onclick="next4()">></strong></h4>
   
    <div id="gif">
       <div id="quiz1"></div>
@@ -156,48 +113,23 @@ function next3(){
         <br>
         <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="Answer()" id="Answer">d)Hamilton</button>
       </div>
+
       <div id="bottom">
-      <button onclick="previou3()" id="previous">Previous Question</button>
       <button onclick="next4()" id="next">Next Question</button>
       <br>
-      <button onclick="Submit()" id="submit">Submit Quiz</button>
       </div>
       <div id="quiz2"></div>
       </div>
       <div id="results"></div>
   </center>`
 }
-// function previous3(){
-//   document.getElementById('main').innerHTML = 
-//   `
-//   <center>
-//   <h2>Where is Hobbiton located in real life?</h2>
-//   <h4><strong onclick="previous2()"><</strong>3/10<strong onclick="next3()">></strong></h4>
-  
-//    <div id="gif">
-//       <div id="quiz1"></div>
-//       <div id="Answers">
-//         <button onclick="answer2()" id="answer2">a)Anaheim</button> <button onclick="answer1()" id="answer1">b)Glendale</button>
-//         <br>
-//         <button onclick="Answer()" id="Answer">c)Alexander Farm</button> <button onclick="answer3()" id="answer3">d)Hamilton</button>
-//       </div>
-//       <div id="bottom">
-//       <button onclick="previous2()" id="previous">Previous Question</button>
-//       <button onclick="next3()" id="next">Next Question</button>
-//       <br>
-//       <!--<button id="submit">Submit Quiz</button> -->
-//       </div>
-//       <div id="quiz2"></div>
-//       </div>
-//       <div id="results"></div>
-//   </center>`
-// }
+
 function next4(){
   document.getElementById('main').innerHTML =
   `
   <center>
   <h2>idk2?</h2>
-  <h4><strong onclick="previous3()"><</strong>5/10<strong onclick="next4()">></strong></h4>
+  <h4><strong><</strong>5/10<strong onclick="next5()">></strong></h4>
   
    <div id="gif">
       <div id="quiz1"></div>
@@ -207,10 +139,9 @@ function next4(){
         <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
       </div>
       <div id="bottom">
-      <button onclick="previous4()" id="previous">Previous Question</button>
+
       <button onclick="next5()" id="next">Next Question</button>
       <br>
-      <button onclick="Submit()" id="submit">Submit Quiz</button>
       </div>
       <div id="quiz2"></div>
       </div>
@@ -221,8 +152,8 @@ function next5(){
   document.getElementById('main').innerHTML =
   `
   <center>
-  <h2>idk3?</h2>
-  <h4><strong onclick="previous45()"><</strong>6/10<strong onclick="next6()">></strong></h4>
+  <h2>Openvraag1?</h2>
+  <h4><strong><</strong>6/10<strong onclick="next6()">></strong></h4>
   
    <div id="gif">
       <div id="quiz1"></div>
@@ -232,8 +163,105 @@ function next5(){
         <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
       </div>
       <div id="bottom">
-      <button onclick="previous5()" id="previous">Previous Question</button>
+
       <button onclick="next6()" id="next">Next Question</button>
+      <br>
+      </div>
+      <div id="quiz2"></div>
+      </div>
+      <div id="results"></div>
+  </center>`
+}
+
+function next6(){
+  document.getElementById('main').innerHTML =
+  `
+  <center>
+  <h2>Openvraag2?</h2>
+  <h4><strong><</strong>7/10<strong onclick="next7()">></strong></h4>
+  
+   <div id="gif">
+      <div id="quiz1"></div>
+      <div id="Answers">
+        <button onclick="Answer()" id="Answer">a)Anaheim</button> <button onclick="answer2()" id="answer2">b)Glendale</button>
+        <br>
+        <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
+      </div>
+      <div id="bottom">
+
+      <button onclick="next7()" id="next">Next Question</button>
+      <br>
+      </div>
+      <div id="quiz2"></div>
+      </div>
+      <div id="results"></div>
+  </center>`
+}
+function next7(){
+  document.getElementById('main').innerHTML =
+  `
+  <center>
+  <h2>Openvraag3?</h2>
+  <h4><strong><</strong>8/10<strong onclick="next8()">></strong></h4>
+  
+   <div id="gif">
+      <div id="quiz1"></div>
+      <div id="Answers">
+        <button onclick="Answer()" id="Answer">a)Anaheim</button> <button onclick="answer2()" id="answer2">b)Glendale</button>
+        <br>
+        <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
+      </div>
+      <div id="bottom">
+
+      <button onclick="next8()" id="next">Next Question</button>
+      <br>
+      </div>
+      <div id="quiz2"></div>
+      </div>
+      <div id="results"></div>
+  </center>`
+}
+function next8(){
+  document.getElementById('main').innerHTML =
+  `
+  <center>
+  <h2>Openvraag4?</h2>
+  <h4><strong><</strong>9/10<strong onclick="next9()">></strong></h4>
+  
+   <div id="gif">
+      <div id="quiz1"></div>
+      <div id="Answers">
+        <button onclick="Answer()" id="Answer">a)Anaheim</button> <button onclick="answer2()" id="answer2">b)Glendale</button>
+        <br>
+        <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
+      </div>
+      <div id="bottom">
+
+      <button onclick="next9()" id="next">Next Question</button>
+      <br>
+      </div>
+      <div id="quiz2"></div>
+      </div>
+      <div id="results"></div>
+  </center>`
+}
+
+function next9(){
+  document.getElementById('main').innerHTML =
+  `
+  <center>
+  <h2>Openvraag5?</h2>
+  <h4><strong><</strong>10/10</h4>
+  
+   <div id="gif">
+      <div id="quiz1"></div>
+      <div id="Answers">
+        <button onclick="Answer()" id="Answer">a)Anaheim</button> <button onclick="answer2()" id="answer2">b)Glendale</button>
+        <br>
+        <button onclick="answer3()" id="answer3">c)Alexander Farm</button> <button onclick="answer1()" id="answer1">d)Hamilton</button>
+      </div>
+      <div id="bottom">
+
       <br>
       <button onclick="Submit()" id="submit">Submit Quiz</button>
       </div>
