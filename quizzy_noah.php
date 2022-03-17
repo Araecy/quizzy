df3d9b5ef833b62007e03d30a655f07348b0f74f
Araecy <74868163+Araecy@users.noreply.github.com>
2022-03-17 14:45:27 +0100

    Cool suss

--- quizzy_noah.php
+++ quizzy_noah.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Noah | Quizzy - Osu</title>
-        <link rel="stylesheet" href="css/styleKewinEdit.sass">
+        <link rel="stylesheet" href="css/noahQuizztStyle.min.css">
         <link rel="stylesheet" href="css/nav_styles.css">
     </head>
     <body>
