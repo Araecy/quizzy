4b63462dda5cf700031215cc45b676c46549246d
87243 <87243@glr.nl>
2022-03-22 21:59:09 +0100

    made prettier
    make code and website much prettier

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="./css/styleIndex.css">
-    <link rel="stylesheet" href="./css/nav_styles.css">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./js/nav_script.js" defer></script>
-    <title>Home</title>
-</head>
-<body>
-    <?php include './nav.php'?>
-<div class="parent">
-<div class="divTop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis reiciendis inventore magnam, iusto nulla in sapiente mollitia fugit provident! Quod perferendis dolores temporibus cum rem exercitationem ullam quaerat est.</div>
-<div class="divMidle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, consectetur accusamus, ea distinctio doloribus dolorum iusto nihil eos commodi eligendi sequi praesentium ratione qui quisquam? Nesciunt recusandae alias similique hic.</div>
-<div class="divLeft"></div>
-<div class="divRight"> </div>
-<div class="divBottom">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?</div>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="./js/nav_script.js" defer></script>
+        <title>Home</title>
+        <link rel="stylesheet" href="./css/indexStyle.css">
+        <link rel="stylesheet" href="./css/nav_styles.css">
+    </head>
+    <body>
+        <?php include './nav.php'?>
+        <div class="parent">
+        <div class="divTop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis reiciendis inventore magnam, iusto nulla in sapiente mollitia fugit provident! Quod perferendis dolores temporibus cum rem exercitationem ullam quaerat est.</div>
+        <div class="divMidle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, consectetur accusamus, ea distinctio doloribus dolorum iusto nihil eos commodi eligendi sequi praesentium ratione qui quisquam? Nesciunt recusandae alias similique hic.</div>
+        <div class="divLeft"></div>
+        <div class="divRight"> </div>
+        <div class="divBottom">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?</div>
+        </div>
+    </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,26 +1,29 @@
-<nav id='menu'>
-  <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
-  <ul>
-    <li><a href='./index.php'>Home</a></li>
-    <li><a class='dropdown-arrow'>Quizzy</a>
-      <ul class='sub-menus'>
-        <li><a href='./kewinsQuizzy.php'>Kewin's quizzy</a></li>
-        <li><a href='./quizzy_duran.php'>Quizzy - Duran</a></li>
-        <li><a href='./quizzy_noah.php'>Quizzy - Noah</a></li>
-      </ul>
-    </li>
-    <li><a href='./aboutUs.php'><li>About us</li></a></li>
-    <li><a href='./contactUs.php'>Contact us</a></li>
-  </ul>
-</nav>
+<div id="menuParent">
+    <nav id='menu'>
+        <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
+        <ul>
+            <li><p>Quizzy</p></li>
+            <li><a href='./index.php'>Home</a></li>
+            <li><a class='dropdown-arrow'>Quizzy</a>
+                <ul class='sub-menus'>
+                    <li><a href='./kewinsQuizzy.php'>Kewin's quizzy</a></li>
+                    <li><a href='./quizzy_duran.php'>Quizzy - Duran</a></li>
+                    <li><a href='./quizzy_noah.php'>Quizzy - Noah</a></li>
+                </ul>
+            </li>
+            <li><a href='./aboutUs.php'><li>About us</li></a></li>
+            <li><a href='./contactUs.php'>Contact us</a></li>
+        </ul>
+    </nav>
+</div>
 
 <script>
-  function updatemenu() {
-    if (document.getElementById('responsive-menu').checked == true) {
-      document.getElementById('menu').style.borderBottomRightRadius = '0';
-      document.getElementById('menu').style.borderBottomLeftRadius = '0';
-    } else {
-      document.getElementById('menu').style.borderRadius = '10px';
+    function updatemenu() {
+        if (document.getElementById('responsive-menu').checked == true) {
+            document.getElementById('menu').style.borderBottomRightRadius = '0';
+            document.getElementById('menu').style.borderBottomLeftRadius = '0';
+        } else {
+            document.getElementById('menu').style.borderRadius = '10px';
+        }
     }
-  }
 </script>
