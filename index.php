5d51e25bb258aa467cf339443d408717cb5336c6
Araecy <74868163+Araecy@users.noreply.github.com>
2022-03-23 22:51:00 +0100

    suc6

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="./js/nav_script.js" defer></script>
-        <title>Quizzy</title>
-        <link rel="stylesheet" href="./css/indexStyle.css">
-        <link rel="stylesheet" href="./css/nav_styles.css">
-    </head>
-    <body>
-        <?php include './nav.php'?>
-        <div class="parent">
-            <div class="divTop">
-                <h1>The Lord Of The Rings Quizzy</h1>
-                <p> <I> <b>made by duran</b> </I></p>
-                <h4>
-                    Kleine introductie quizzy LOTR:
-                    Het begon met het smeden van de Grote Ringen. Drie werden aan de Elfen gegeven, onsterfelijk, wijs en eerlijk van alle wezens. Seven voor de Dwarf-Lords, grote mijnwerkers en ambachtslieden van de berghallen. En negen, negen ringen werden geschonken aan het ras van mannen, die boven alles naar macht verlangen.
-                </h4>
-            </div>
-            <div class="divLeft">
-            <center>
-            <a href='./quizzy_duran.php'><button>Start quiz</button></a>
-            </center>
-            </div>
-            <div class="divMiddle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, consectetur accusamus, ea distinctio doloribus dolorum iusto nihil eos commodi eligendi sequi praesentium ratione qui quisquam? Nesciunt recusandae alias similique hic.
-            </div>
-            <div class="divRight">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore fugiat nisi praesentium, ipsum quisquam possimus voluptatum? Sequi inventore facilis laudantium! Quo, rem facilis quae ad odio ab eligendi maiores.
-            </div>
-            <div class="divBottom">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?
-            </div>
-            <div class="div6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?
-            </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./js/nav_script.js" defer></script>
+    <title>Quizzy</title>
+    <link rel="stylesheet" href="./css/indexStyle.css">
+    <link rel="stylesheet" href="./css/nav_styles.css">
+</head>
+
+<body>
+    <?php include './nav.php' ?>
+    <div class="parent">
+        <div class="divTop">
+            <h1>The Lord Of The Rings Quizzy</h1>
+            <p> <I> <b>made by duran</b> </I></p>
+            <h4>
+                Kleine introductie quizzy LOTR:
+                Het begon met het smeden van de Grote Ringen. Drie werden aan de Elfen gegeven, onsterfelijk, wijs en eerlijk van alle wezens. Seven voor de Dwarf-Lords, grote mijnwerkers en ambachtslieden van de berghallen. En negen, negen ringen werden geschonken aan het ras van mannen, die boven alles naar macht verlangen.
+            </h4>
         </div>
-    </body>
+        <div class="divLeft">
+            <center>
+                <a href='./quizzy_duran.php'><button>Start quiz</button></a>
+            </center>
+        </div>
+        <div class="divMiddle">
+        osu! is a free-to-play rhythm game primarily developed, published, and created by Dean "peppy" Herbert. Inspired by iNiS' rhythm game Osu! Tatakae! Ouendan, it was written in C# on the .NET framework, and was released for Microsoft Windows on 16 September 2007.
+        </div>
+        <div class="divRight">
+            <a href='./quizzy_noah.php'><button>Start quiz</button></a>
+        </div>
+        <div class="divBottom">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?
+        </div>
+        <div class="div6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui, voluptatibus doloremque saepe ipsam, repellendus porro tempora praesentium veniam magni natus placeat aliquam fugiat minus. Neque temporibus repellat natus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae hic facere nulla delectus quia ipsam debitis suscipit! Nobis aut hic necessitatibus delectus quae, possimus ipsa, nostrum facere minus maiores id?
+        </div>
+    </div>
+</body>
+
 </html>
